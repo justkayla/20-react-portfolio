a849b5826ca0a0a8078be981f93e766f27cd1573
Kayla Justice <kaylarjustice@gmail.com>
2022-12-05 13:41:37 -0600

    Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,23 +9,23 @@ const Contact = ({ title }) => {
     <>
       <Subheader title={"Contact me"} />
       {/* <ContactForm /> */}
-      <Container style={{ display: "flex", justifyContent: "center" }}>        
+      <Container style={{ display: "flex", justifyContent: "center" }}>
         <Stack>
           <center>
-          <p style={{ marginTop: "20px" }}>
-            <a mailto="kaylarjustice@gmail.com">
-              <strong>Email:</strong> kaylarjustice@gmail.com
-            </a>
-          </p>
-          <p>
-            <strong>Phone:</strong> 713-203-9870
-          </p>
-          <img
-            src={require("../assets/images/profilepic1.jpg")}
-            style={{ width: "350px", borderRadius: "10%", marginTop: "0px" }}
-          ></img>          
+            <p style={{ marginTop: "20px" }}>
+              <a mailto="kaylarjustice@gmail.com">
+                <strong>Email:</strong> kaylarjustice@gmail.com
+              </a>
+            </p>
+            <p>
+              <strong>Phone:</strong> 713-203-9870
+            </p>
+            <img
+              src={require("../assets/images/profilepic1.jpg")}
+              style={{ width: "350px", borderRadius: "10%", marginTop: "0px" }}
+            ></img>
           </center>
-        </Stack>        
+        </Stack>
       </Container>
     </>
   );
